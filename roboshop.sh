303e282b45e8e75bb4b37f94161bd4a8a6534a72
kirankumar kosuri <kosurikirankumar7@gmail.com>
2025-10-12 16:40:27 +0530

    roboshop.sh

--- roboshop.sh
+++ roboshop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="sg-07c8acf3fa6b923fa" # replace with your SG ID
-ZONE_ID="Z0948150OFPSYTNVYZOY" # replace with your ID
-DOMAIN_NAME="daws86s.fun"
+SG_ID="sg-0554fc62c9a958751" # replace with your SG ID
+ZONE_ID="Z09644902QVXLMBTH3X1Z" # replace with your ID
+DOMAIN_NAME="daws86s.blog"
 
 for instance in $@ # mongodb redis mysql
 do
@@ -12,10 +12,10 @@ do
     # Get Private IP
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
-        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws86s.fun
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws86s.blog
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-        RECORD_NAME="$DOMAIN_NAME" # daws86s.fun
+        RECORD_NAME="$DOMAIN_NAME" # daws86s.blog
     fi
 
     echo "$instance: $IP"
